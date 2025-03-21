734a1faa78572b0d10ad372dc4e99e4f230bae72
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-03-22 00:21:56 +0300

    Added Car class with subclasses (Ford, Holden, Mitsubishi) and implemented basic methods (startEngine, accelerate, brake)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import org.example.company.Car;
+import org.example.company.Ford;
+import org.example.company.Mitsubishi;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car(8 , "Base Car");
+        car.startEngine();
+        car.accelerate();
+        car.brake();
+        System.out.println("********************");
+        Car mt = new Mitsubishi(6 , "Outlander");
+        mt.startEngine();
+        mt.accelerate();
+        mt.brake();
+        System.out.println("********************");
+        Car ford = new Ford(4, "Focus");
+        ford.startEngine();
+        ford.accelerate();
+        ford.brake();
+
+
     }
 }
